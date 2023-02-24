dab156314256c6bae4e0529f4432e6993d658498
Michael Sol <Sol.mike.j@gmail.com>
2023-02-24 08:15:37 -0800

    Create welcome message and intake user order

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,7 +1,18 @@
+using System;
 class Program
 {
   static void Main()
   {
     // We'll add user interface code here.
+  WelcomeMessage();
+  }
+
+  static void WelcomeMessage()
+  {
+    Console.WriteLine("Hello! \nHow many pieces of bread would you like?");
+    int numberOfBread = int.Parse(Console.ReadLine());
+    Console.WriteLine("Okay, " + numberOfBread + " pieces of bread coming up.");
+    Console.WriteLine("How many pastries would you like?");
+    int numberOfPastry = int.Parse(Console.ReadLine());
   }
 }
